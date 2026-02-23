a1ccd0cf2c45b7ca8521b9d1e6f098a6ec86b7b2
unknown <00sgor@gmail.com>
2026-02-23 16:28:49 -0500

    Cree las carpetas para el atomic design y temine los atomos de el login

---
+++ Frontend/src/components/atoms/buttonSave.jsx
@@ -0,0 +1,22 @@
+export const ButtonSave=()=>{
+    return(
+        <button type="submit" className="login-button" style={styles}>
+            Ingresar
+        </button>
+    )
+}
+
+
+
+const styles = {
+    width: "100%",
+    padding: "12px",
+    backgroundColor: "#39A900",
+    color: "white",
+    border: "none",
+    borderRadius: "8px",
+    fontWeight: "bold",
+    cursor: "pointer",
+};
+
+
---
+++ Frontend/src/components/atoms/messageExito.jsx
@@ -0,0 +1,19 @@
+export const MessageExito=({text})=>{
+    return(
+        <p style={styles}>{text}</p>
+
+    )
+}
+
+
+const styles={
+    marginTop: "10px",
+    padding: "10px 12px",
+    backgroundColor: "#e6f4ea",      
+    color: "#1e7f43",                 
+    border: "1px solid #b7e1c1",
+    borderRadius: "6px",
+    fontSize: "14px",
+    fontWeight: "500",
+    textAlign: "center"
+}
